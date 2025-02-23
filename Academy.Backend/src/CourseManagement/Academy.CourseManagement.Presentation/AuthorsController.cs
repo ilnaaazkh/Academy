a12a2bd40a51333c00af03e2e0965a5f13647736
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-02-23 20:20:01 +0300

    Modular monolith project structure

---
+++ Academy.Backend/src/CourseManagement/Academy.CourseManagement.Presentation/AuthorsController.cs
@@ -0,0 +1,14 @@
+﻿using Academy.Framework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academy.CourseManagement.Presentation
+{
+    public class AuthorsController : ApplicationController
+    {
+        [HttpGet]
+        public ActionResult HelloWorld()
+        {
+            return Ok("dscdsdcs");
+        }
+    }
+}
---
+++ Academy.Backend/src/Shared/Academy.Framework/ApplicationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academy.Framework
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationController : ControllerBase
+    {
+    }
+}
