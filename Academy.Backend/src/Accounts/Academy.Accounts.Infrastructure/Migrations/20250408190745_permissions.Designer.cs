571950bc749ac4123621f6b9b2d042989f665340
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-04-08 22:14:38 +0300

    Migration

--- Academy.Backend/src/Accounts/Academy.Accounts.Infrastructure/Migrations/20250408190745_permissions.Designer.cs
+++ Academy.Backend/src/Accounts/Academy.Accounts.Infrastructure/Migrations/20250408190745_permissions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Academy.Accounts.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Academy.Accounts.Infrastructure.Migrations
 {
     [DbContext(typeof(AccountsDbContext))]
-    partial class AccountsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250408190745_permissions")]
+    partial class permissions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
